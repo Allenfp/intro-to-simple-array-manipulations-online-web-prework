77c5a56b8cbd4d1161c4f4d708da2a2a6ff55098
Allen F Partlow <allenfp@gmail.com>
2019-01-26 19:33:19 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,44 @@
+def using_push(array, string)
+    array.push(string)
+end
+
+def using_unshift
+  
+end
+
+def using_pop
+  
+end
+
+def pop_with_args
+  
+end
+
+def using_shift
+  
+end
+
+def shift_with_args
+  
+end
+
+def using_concat
+  
+end
+
+def insert
+  
+end
+
+def using_uniq
+
+end
+
+def using_flatten
+  
+end
+
+def using_delete
+  
+end
+
